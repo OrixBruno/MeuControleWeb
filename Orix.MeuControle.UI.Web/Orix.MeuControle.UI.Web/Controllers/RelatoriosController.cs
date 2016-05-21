6880dec13561f09af624cb29c2b88f290cbe5dd4
Tibiotas <blade-falcon@hotmail.com>
2016-05-21 12:42:56 -0300

    CommitInicial

---
+++ Orix.MeuControle.UI.Web/Orix.MeuControle.UI.Web/Controllers/RelatoriosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Orix.MeuControle.UI.Web.Controllers
+{
+    public class RelatoriosController : Controller
+    {
+        // GET: Relatorios
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+        public ActionResult Editar()
+        {
+            return View();
+        }
+    }
+}
---
+++ Orix.MeuControle.UI.Web/Orix.MeuControle.UI.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Orix.MeuControle.UI.Web.MvcApplication" Language="C#" %>
