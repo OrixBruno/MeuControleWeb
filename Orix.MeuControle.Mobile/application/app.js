5c747cf4e07ab80209c0b39e0a5598645b7af736
Tibiotas <blade-falcon@hotmail.com>
2016-09-28 17:01:31 -0300

    Opcoes e menu lateral

--- Orix.MeuControle.Mobile/application/app.js
+++ Orix.MeuControle.Mobile/application/app.js
@@ -1,1 +1,7 @@
-﻿
+﻿var app = angular.module("MEUCONTROLE", ['ionic', 'ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider.when("/", { templateUrl: 'application/views/home', controller: 'homeController' });
+
+    $routeProvider.otherwise({ redirectTo: "/" });
+});
